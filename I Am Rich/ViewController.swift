b805d2f3a26a72eb71f56bbd4f12f3ed3471c0de
Sai Yalamarty <sai1494@yahoo.com>
2020-06-20 02:48:47 -0500

    one last time

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Sai Sharan Yalamarty on 6/19/20.
+//  Copyright © 2020 Sai Sharan Yalamarty. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
